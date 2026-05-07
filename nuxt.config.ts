6c93ccd6296199fab815bd7bdb0e58912709442f
Kazuto <mail@kazuto.de>
2026-05-07 16:08:00 +0200

    feat(UI): Add Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,18 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon"],
 
+  css: ["./app/assets/css/main.css"],
+
   typescript: {
     typeCheck: true,
   },
-});
 
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
